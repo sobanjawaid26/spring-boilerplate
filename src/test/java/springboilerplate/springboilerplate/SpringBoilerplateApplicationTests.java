5c88d4827b26c1251f4832328ad28ab15ce473ac
sobanjawaid26 <sobanjawaid26@gmail.com>
2020-11-07 17:15:09 +0530

    first commit

---
+++ src/test/java/springboilerplate/springboilerplate/SpringBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package springboilerplate.springboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
